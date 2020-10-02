d1aa43c341918a47a81aafb2323ef20f229c9f66
leticia.pfaltzgraff <leticia.pfaltzgraff@m4u.com.br>
2020-10-01 23:47:50 -0300

    Retornando mensagens antigas e usuário que entra.

--- chat_client_tcp.js
+++ chat_client_tcp.js
@@ -1,16 +1,37 @@
 const socket = require('socket.io-client')('http://localhost:3000');
 const repl = require('repl');
 const chalk = require('chalk');
+const listaDeUsuarios = require('./usuarios.json');
+const listaDeMensagens = require('./mensagens.json');
 
 let usuario = null;
 socket.on('connect', ()=>{
   usuario = process.argv[2];
+
+  for(let msg of listaDeMensagens){
+    let usuarioDaMensagem = listaDeUsuarios.find(u => u.id == msg.usuario);
+    let usuarioLogado = listaDeUsuarios.find(u => u.nome = usuario);
+    
+    if(usuarioDaMensagem.grupo == usuarioLogado.grupo){
+      console.log(chalk.white(`${usuarioDaMensagem.nome} > ${msg.texto}`));
+    }
+  }
+
   console.log(chalk.red(`usuario ${usuario} acabou de entrar`));
+
+  socket.emit('user_entry_notification', {
+    usuario
+  });
 });
 
 socket.on('message', (event)=>{
   const {mensagem, usuario} = event
+
   console.log(chalk.green(`${usuario} > ${mensagem}`));
+});
+
+socket.on('user_entry_notification', (event)=>{
+  console.log(chalk.red(event));
 });
 
 repl.start({
--- chat_server_tcp.js
+++ chat_server_tcp.js
@@ -6,6 +6,13 @@ http.listen(3000, () => { console.log('estou ouvindo') });
 io.on('connection', (socket)=>{
   console.log('cliente conectado', socket.id);
 
+  socket.on('user_entry_notification', (event)=>{
+    let notification = `usuario ${event.usuario} acabou de entrar`;
+    console.log(notification)
+    socket.broadcast.emit('user_entry_notification', notification);
+  });
+  
+
   socket.on('message', (event)=>{
     console.log(event);
     socket.broadcast.emit('message', event);
